fa520c64611a09cb1f9a02ad853f9daebf346555
Evan Weinberg <weinbergmath@emwcenter.local>
2017-12-25 16:01:38 -0500

    initial
    This is my work on the Segues activity in the Chapter 3, Section 6
    activity from App Development with Swift. I can’t figure out why the
    unwind segue isn’t working. Can you?

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Evan Weinberg on 7/26/17.
+//  Copyright © 2017 Evan Weinberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+   
+  
+    
+ 
+    @IBAction func goBackToMain(for segue: UIStoryboardSegue, sender: Any?) {
+        
+         print("fired")
+    }
+        
+    
+
+
+}
+
